303173c37fef83af24b11f193869d44a3b92d66c
Marwane Bidamou <devr.marwane@gmail.com>
2023-02-04 23:17:52 +0100

    Initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService1;
+using GrpcService1.Protos;
+
+
+
+
+var channel = GrpcChannel.ForAddress("https://localhost:7042");
+
+
+//1st example
+var client = new Greeter.GreeterClient(channel);
+
+var input = new HelloRequest { Name = "Marwan" };
+var reply = await client.SayHelloAsync(input);
+
+Console.WriteLine($"Hello: {reply}");
+
+
+//2nd example
+var client2 = new Customer.CustomerClient(channel);
+
+Console.WriteLine("Enter the id of the customer (1,2 or 3)");
+var id = int.Parse(Console.ReadLine());
+
+var result = await client2.GetCustomerInfoAsync(new CustomerLookupModel { UserId = id });
+Console.WriteLine($"name: {result.FirstName} {result.LastName}");
+Console.WriteLine($"email: {result.EmailAddress}");
+
+Console.WriteLine();
+Console.WriteLine("********* new customer list ***********");
+Console.WriteLine();
+
+//3rd example
+using (var call = client2.GetNewCustomer(new NewCustomerRequest()))
+{
+    while(await call.ResponseStream.MoveNext())
+    {
+        var currentCustomer = call.ResponseStream.Current;
+        Console.WriteLine($"current customer =>name: {currentCustomer.FirstName} {currentCustomer.LastName} - {currentCustomer.EmailAddress}");
+    }
+}
+
+Console.ReadLine();
---
+++ GrpcService1/Services/CustomersService.cs
@@ -0,0 +1,86 @@
+﻿using Grpc.Core;
+using GrpcService1.Protos;
+
+namespace GrpcService1.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            }
+            else if (request.UserId == 3)
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+
+            return Task.FromResult(output);
+
+        }
+
+        public override async Task GetNewCustomer(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Tim",
+                    LastName = "cock",
+                    Age=29,
+                    EmailAddress = "tom cock",
+                    IsAlive = true,
+                },
+                new CustomerModel
+                {
+                    FirstName = "Nas",
+                    LastName = "Edd",
+                    Age=29,
+                    EmailAddress = "nass.edd@gmail.com",
+                    IsAlive = true,
+                },
+                new CustomerModel
+                {
+                    FirstName = "Tom",
+                    LastName = "Mark",
+                    Age=29,
+                    EmailAddress = "tom.mark@gmail.com",
+                    IsAlive = true,
+                },
+                new CustomerModel
+                {
+                    FirstName = "Nancy",
+                    LastName = "Albert",
+                    Age=29,
+                    EmailAddress = "nancy.albert@gmail.com",
+                    IsAlive = true,
+                }
+            };
+
+            foreach(var item in customers)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(item);
+            }
+        }
+    }
+}
+ 
